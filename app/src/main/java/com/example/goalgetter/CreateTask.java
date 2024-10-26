2826c82c969a75da682573361bb24d44a2d6a1c1
DanielGals <janjangallaron@gmail.com>
2024-10-27 00:24:25 +0800

    Added Date, Deadline, Time

--- app/src/main/java/com/example/goalgetter/CreateTask.java
+++ app/src/main/java/com/example/goalgetter/CreateTask.java
@@ -1,5 +1,8 @@
 package com.example.goalgetter;
 
+import android.app.AlarmManager;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,11 +10,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import android.view.MotionEvent;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+
+import java.util.Calendar;
 
 public class CreateTask extends AppCompatActivity {
 
     private ImageButton backButton;
+
+    EditText courseNameEditText;
+    EditText taskTypeEditText;
+    EditText descriptionEditText;
+    CheckBox priorityModeCheckBox;
+    ImageView uploadImageView;
+    Button uploadImageButton;
+
+    EditText dateStartedEditText;
+    EditText deadlineEditText;
+    EditText alarmEditText;
+
+
+
+
+    Button createTaskButton;
+
+    int mYear, mMonth, mDay;
+    int mHour, mMinute;
+    AlarmManager alarmManager;
+    TimePickerDialog timePickerDialog;
+    DatePickerDialog datePickerDialog;
+    MainActivity activity;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +55,108 @@ public class CreateTask extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_create_task);
 
-        backButton = findViewById(R.id.back_button);
+        courseNameEditText = findViewById(R.id.courseNameEditText);
+        taskTypeEditText = findViewById(R.id.taskTypeEditText);
+        descriptionEditText = findViewById(R.id.descriptionEditText);
+        priorityModeCheckBox = findViewById(R.id.priorityModeCheckBox);
 
+        dateStartedEditText = findViewById(R.id.dateStartedEditText);
+        deadlineEditText = findViewById(R.id.deadlineEditText);
+        alarmEditText = findViewById(R.id.alarmEditText);
+
+
+
+        uploadImageView = findViewById(R.id.uploadImageView);
+
+
+        uploadImageButton = findViewById(R.id.uploadImageButton);
+        createTaskButton = findViewById(R.id.createTaskButton);
+
+
+
+        dateStartedEditText.setOnTouchListener((view, motionEvent) -> {
+            if(motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                final Calendar c = Calendar.getInstance();
+                mYear = c.get(Calendar.YEAR);
+                mMonth = c.get(Calendar.MONTH);
+                mDay = c.get(Calendar.DAY_OF_MONTH);
+                datePickerDialog = new DatePickerDialog(this,
+                        (view1, year, monthOfYear, dayOfMonth) -> {
+                            dateStartedEditText.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                            datePickerDialog.dismiss();
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
+                datePickerDialog.show();
+            }
+            return true;
+        });
+
+        deadlineEditText.setOnTouchListener((view, motionEvent) -> {
+            if(motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                final Calendar c = Calendar.getInstance();
+                mYear = c.get(Calendar.YEAR);
+                mMonth = c.get(Calendar.MONTH);
+                mDay = c.get(Calendar.DAY_OF_MONTH);
+                datePickerDialog = new DatePickerDialog(this,
+                        (view1, year, monthOfYear, dayOfMonth) -> {
+                            deadlineEditText.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                            datePickerDialog.dismiss();
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
+                datePickerDialog.show();
+            }
+            return true;
+        });
+
+        alarmEditText.setOnTouchListener((view, motionEvent) -> {
+            if(motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                // Get Current Time
+                final Calendar c = Calendar.getInstance();
+                mHour = c.get(Calendar.HOUR_OF_DAY);
+                mMinute = c.get(Calendar.MINUTE);
+
+                // Launch Time Picker Dialog
+                timePickerDialog = new TimePickerDialog(this,
+                        (view12, hourOfDay, minute) -> {
+                            alarmEditText.setText(hourOfDay + ":" + minute);
+                            timePickerDialog.dismiss();
+                        }, mHour, mMinute, false);
+                timePickerDialog.show();
+            }
+            return true;
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        backButton = findViewById(R.id.back_button);
         backButton.setOnClickListener(v -> {
             finish();
         });
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
     }
 }
