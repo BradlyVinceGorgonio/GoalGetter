dc61a7b977c8c4d8ee51b0cf735b532b0921718c
DanielGals <janjangallaron@gmail.com>
2024-11-21 14:40:49 +0800

    test 2

--- app/src/main/java/com/example/goalgetter/MainActivity.java
+++ app/src/main/java/com/example/goalgetter/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.goalgetter;
 
+import android.app.AlarmManager;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,6 +26,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        // Check and request exact alarm permission on Android 12 (API 31) or higher
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+            if (!alarmManager.canScheduleExactAlarms()) {
+                // Launch the settings screen to request permission
+                Intent intent = new Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM);
+                startActivity(intent);
+            }
+        }
         new Handler().postDelayed(() -> {
             Intent intent = new Intent(MainActivity.this, Login.class);
             startActivity(intent);
